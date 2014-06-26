c4bb488ffa9e687570f2c985d4313d88c89c6e86
Kevin Gladhart <knglad@email.arizona.edu>
2014-06-25 18:06:23 -0700

    drive object class created
    will contain information for a single drive on the system, Storage
    being an example.

---
+++ Data_Backup_dbet/src/engine/Drive.java
@@ -0,0 +1,20 @@
+package engine;
+
+/**
+ * 
+ * @author kevin
+ *
+ *
+ *	This class will allow us to create a Drive object that is able to read a single drive and save its information
+ *	in variables, things like Name, mount point, overall storage capacity, free storage and used storage, even file system. 
+ */
+
+public class Drive {
+	
+	public Drive(String name, String mount_point, double total_capac, double free_capac, double used_capac, String fs){
+		// Might need to add an object that references that drive, then call the methods to set each one or calculate it
+		//	 so that its in human readable GB.  
+		
+	}
+
+}
