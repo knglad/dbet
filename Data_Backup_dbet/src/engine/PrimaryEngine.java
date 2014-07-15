78fae9ec8939fc9885d9126dac2827cc629d7ef2
Kevin Gladhart <knglad@email.arizona.edu>
2014-07-15 15:50:50 -0700

    rework of the primary engine, added some tests and made them pass

--- Data_Backup_dbet/src/engine/PrimaryEngine.java
+++ Data_Backup_dbet/src/engine/PrimaryEngine.java
@@ -1,5 +1,8 @@
 package engine;
 
+import java.io.File;
+import java.util.ArrayList;
+
 /**
  * 
  * @author kevin
@@ -7,11 +10,28 @@ package engine;
  *	This class is used to determine what subclasses are needed to be ran.
  * Things like determining which OS we are using and if we want a FRONT or BACK
  * room MODE. 
+ * 
+ * TODO 
+ * 	Find Java mount point for windows systems for 'File windows' in primary engine constructor
+ * 	Make list of key words to ignore certain drives or images of things that aren't backup drives ( LM PNP for example ). 
  */
 
 public class PrimaryEngine {
 
+
 	public String OS; // The Operating System type we are working with
+	private ArrayList<File> rawDrives = new ArrayList<File>(); // Arraylists make themselves bigger if need be, automatically. 
+	private boolean listMadeSuccessfully = false;
+	
+	/**
+	 * MANUAL HARD CODED DRIVE STRINGS ARE HERE! EDIT IF MOUNT POINTS OR NAMES CHANGE!!!!!!!!
+	 */
+	// I couldn't find a dynamic way to get ALL the drives 
+			// NOTE: macVolumes must be the absolute path to EACH PARTITIONS ROOT!! Breaks otherwise and doesn't know what to do.
+	private String[] macVolumes = { "/Volumes/Storage/", "/Volumes/Storage2/", "/Volumes/Storage3/"};
+	private String[] windowsVolumes = {"D:/"}; // EDIT THIS WHEN WE KNOW WHAT LETTER IS ATTACHED TO THE WINDOWS BACKUP DRIVES!!
+	
+	
 	
 	
 	
@@ -19,22 +39,48 @@ public class PrimaryEngine {
 		// Determine which OS we are working with
 	 OS = System.getProperty("os.name");
 	 
-	 if (OS.equals("Mac OS X")){
-		 macSystem();
+	 if ( OS.equals("Mac OS X") ){
+		listMadeSuccessfully =  makeDriveList(macVolumes);
 	}
+	 else if ( OS.contains("Windows") ){ // CONFIRM THIS ON A WINDOWS MACHINE!!!
+		 listMadeSuccessfully = makeDriveList(windowsVolumes);
+		 
+	 }
 	 
-}
+}	
+	
+	/**
+	 * 
+	 * @param file 
+	 * 		a file that is the mount point for all the drives, /Volumes/ for example
+	 * @return
+	 * 		false if an empty list or true if something is in the list.
+	 */
+	public boolean makeDriveList(String[] mountPoint){
+		
+		for (String s : mountPoint){// Advanced for loop, for each file I'm calling f WITHIN fileArray, do the following.
+			
+			File f = new File(s); // Make a file out of the string mount point
+			rawDrives.add(f); // add this newly created file to the list, the next available position.
+		}
+		
+		if (rawDrives.size() == 0 || rawDrives.size() < mountPoint.length)
+			return false; // its empty, it can never be empty
+		else 
+			return true; // there's something in it, that's all this cares about. 
+	}
+	
+	
 	public String getOS(){
-		System.out.println(OS);
 		return OS;
 	}
 	
-	public void macSystem(){
-		// If its a Mac do this
+	public boolean isListMade(){
+		return listMadeSuccessfully;
 	}
 	
-	public void WindowsSystem(){
-		// If its Windows do this
+	public ArrayList<File> getDriveList(){
+		return rawDrives;
 	}
 	
-}
+} // END OF PRIMARY ENGINE
--- Data_Backup_dbet/src/tests/engineTest.java
+++ Data_Backup_dbet/src/tests/engineTest.java
@@ -2,6 +2,8 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Test;
 
 import engine.PrimaryEngine;
@@ -13,6 +15,18 @@ public class engineTest {
 		PrimaryEngine pe = new PrimaryEngine();
 		
 		assertEquals("Mac OS X", pe.getOS() ); // This will fail on Windows
+		// Test to ensure the list is populating in engine
+		assertTrue(pe.isListMade());
 		
+		// Test to see how many drives are in the array list
+		assertEquals(3, pe.getDriveList().size());
+		assertEquals("/Volumes/Storage", pe.getDriveList().get(0).getPath() ); // It knows its a folder, drops the last '/'
+			// Also the getName() returns the folder we are in with this file particularly. getPath returns the entire path for the file.
+		
+		assertEquals("/Volumes/Storage2", pe.getDriveList().get(1).getPath() );
+		
+		// Check to ensure they have specific free space remaining
+		assertTrue(   pe.getDriveList().get(0).getFreeSpace() != pe.getDriveList().get(1).getFreeSpace()   );
+
 	}
 }
