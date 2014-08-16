1c55de170ac9df51e1cd3fe128caf42e06744c22
Kevin Gladhart <knglad@email.arizona.edu>
2014-08-15 17:58:50 -0700

    small work on backupEngine

--- Data_Backup_dbet/src/engine/BackupEngine.java
+++ Data_Backup_dbet/src/engine/BackupEngine.java
@@ -2,28 +2,32 @@ package engine;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * @author Kevin Gladhart
- *         <p/>
+ *
  *         This engine determines the logic on how to backup the given drive in the most effective way
  */
 public class BackupEngine {
 
     public String OS;
+    public DisregardDrives dd;
+    public ArrayList<Drive> drivesToBackup;
 
     public BackupEngine(String givenOperatingSystem) {
 
         OS = givenOperatingSystem;
 
         // Load in the list of drives to ignore
-        DisregardDrives dd = new DisregardDrives();
         dd = dd.loadList(); // Primary engine adds the list prior to this
 
         // -----------------------------------------------
         // Find the drives to backup
         findDrivesToBackup(dd);
 
+        // Ask which ones to backup, make a list
+        // Options: Yes, No, Don't Ask Again
         // Determine best method to back them up
         // Back them up
         // Log everything to text file and GUI element
@@ -33,22 +37,31 @@ public class BackupEngine {
     /**
      * @param driveList - all the drives we DONT backup, in the form of ArrayList<String>
      * @return A new arrayList with the drives we DO want to backup
-     * <p/>
+     *
      * This will look through the systems entire mount-points and find ones that we may care
      * about and return the list.
      */
-    public ArrayList<Drive> findDrivesToBackup(DisregardDrives driveList) {
+    public ArrayList<File> findDrivesToBackup(DisregardDrives driveList) {
 
-        File[] currentDrives;
+        ArrayList<File> currentDrives = null;
+        File f = null;
 
-        if (OS.contains("Mac")) {
-            File f = new File("/Volumes");
-            currentDrives = f.listFiles();
+        if (OS.contains("Mac"))
+            f = new File("/Volumes/");
+
+        else if (OS.contains("Windows"))
+            f = new File("Computer");
+
+        currentDrives = new ArrayList<File>(Arrays.asList(f.listFiles()));
+
+        for (File file : currentDrives) {
+            if (dd.contains(file.getName())) {
+                currentDrives.remove(file);
+            }
         }
 
-
-        return null;
+        return currentDrives;
     }
 
 
-}
+} // END OF BACKUP ENGINE
